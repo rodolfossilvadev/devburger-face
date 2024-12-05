62572e25093b8800a173b56f44e3d34db8c76750
rodolfossilvadev <rodolfodev30@gmail.com>
2024-12-04 22:27:52 -0300

    rodando api no render com o netfly

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3001'
+    baseURL: 'https://devburgerapi.onrender.com'
 });
 
 api.interceptors.request.use((config) => {
